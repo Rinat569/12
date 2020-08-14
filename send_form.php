723eae23997e3a676bcd073a17c9604c7457332a
Rinat <58187889+Rinat569@users.noreply.github.com>
2020-08-14 22:37:23 +0400

    Add files via upload

---
+++ send_form.php
@@ -0,0 +1,10 @@
+<?php
+$phone= $_POST['phone'];
+$email= $_POST['email'];
+$adres= $_POST['adres'];
+$emailTo = 'sky2208@ya.ru'; //Сюда введите Ваш email
+$body = "$fio \n\n$phone\n\n$email \n\n$adres";
+$headers = "Content-Type: text/plain; charset=utf-8\r\n".'From: MySite <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $emailTo;
+mail($emailTo, $fio, $body, $headers);
+$emailSent = true;
+?>
